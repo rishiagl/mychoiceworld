35fc2c0f1c000bac8eff7e295f1dbd602909bafc
Rishi Agarwal <rishi.agl@outlook.com>
2024-06-27 23:31:39 +0530

    Catalog admin panel completed

--- core/catalog/admin.py
+++ core/catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Category, Product, GstClassification
+
+from django.utils.text import slugify
+
+admin.site.site_header = "CERMS Administration Panel"
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shortName')
+
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shortName')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'brand', 'category')
+    readonly_fields = ('name',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:  # This is an add operation
+            return ()
+        return self.readonly_fields
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class GstClassificationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'type', 'rate')
+
+    def name(self, obj):
+        return obj
+
+
+admin.site.register(GstClassification, GstClassificationAdmin)
--- core/catalog/models.py
+++ core/catalog/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib import admin
+from django.forms import ValidationError
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    shortName = models.CharField(max_length=5, unique=True, editable=True)
+
+    def save(self, *args, **kwargs):
+        self.name = slugify(self.name).upper()
+        self.shortName = self.shortName.upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    shortName = models.CharField(max_length=5, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.name = slugify(self.name).upper()
+        self.shortName = self.shortName.upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    model = models.CharField(max_length=100, blank=True, editable=True)
+    brand = models.ForeignKey(Brand, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.SlugField(max_length=100, unique=True,
+                            blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.name = slugify(self.brand.shortName + '-' +
+                            self.category.shortName + '-' + self.model).upper()
+        self.model = slugify(self.model).upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+def validate_Code(value):
+    if not value.isdigit():
+        raise ValidationError(f"{value} is not an integer.")
+
+
+class GstClassification(models.Model):
+    Type_Choices = (
+        ('HSN', 'Goods'),
+        ('SAC', 'Services'),
+    )
+
+    Rate_Choices = (
+        (6, '6%'),
+        (12, '12%'),
+        (18, '18%'),
+        (28, '28%'),
+    )
+
+    type = models.CharField(
+        max_length=10, choices=Type_Choices, default='HSN')
+
+    code = models.CharField(max_length=8, unique=True,
+                            validators=[validate_Code])
+    description = models.TextField(max_length=100, blank=True)
+    rate = models.IntegerField(choices=Rate_Choices, default='18')
+
+    def __str__(self):
+        return self.type + '-' + self.code + '-' + str(self.rate)
