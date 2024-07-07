134f8fe418358451904545e08279f68cfc799783
Rishi Agarwal <rishi.agl@outlook.com>
2024-07-07 15:38:37 +0530

    home changed

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from home import views
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
+    path('', views.index, name='index'),
     path('home/', include("home.urls")),
     path('catalog/', include("catalog.urls")),
     path('admin/', admin.site.urls),
