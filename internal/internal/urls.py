eaef1370e01e8b7774e25406e816c4d9ff0ae266
Rishi Agarwal <rishi.agl@outlook.com>
2024-07-22 18:51:46 +0530

    Main site edited

--- internal/internal/urls.py
+++ internal/internal/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("__reload__/", include("django_browser_reload.urls")),
     path('marketing/', include("marketing.urls")),
     path('catalog/', include("catalog.urls")),
