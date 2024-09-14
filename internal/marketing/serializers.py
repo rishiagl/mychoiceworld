09abea390dc3508ec094bd46db33524ebe29f8b6
Rishi Agarwal <rishi.agl@outlook.com>
2024-09-14 12:54:18 +0530

    repo cleaned

--- internal/marketing/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from marketing.models import CallbackRequests
-
-
-class CallbackRequestsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CallbackRequests
-        fields = ['created_on', 'customer_name', 'phone_no',
-                  'remark', 'is_starred', 'is_closed', 'last_updated']
